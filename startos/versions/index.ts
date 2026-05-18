7985193a725009911dacba5869f6f2297e843532
Matt Hill <mattnine@protonmail.com>
2026-05-18 09:07:13 -0600

    Bump version to 2.7.5:5

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_2_7_5_4 } from './v2.7.5.4'
+import { v_2_7_5_5 } from './v2.7.5.5'
 
 export const versionGraph = VersionGraph.of({
-  current: v_2_7_5_4,
+  current: v_2_7_5_5,
   other: [],
 })
