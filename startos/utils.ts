Start9Labs/cal-diy-startos

chore: migrate to start-sdk 2.0
Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01F5qZh4QiVySBLkV5ya7XSr

startos/utils.ts
@@ -6,13 +6,23 @@ export const postgresPort = 5432
 export const postgresUser = 'calcom'
 export const postgresDb = 'calendso'
 
+// Host id (the `sdk.MultiHost.of` group) vs. the interface id exported on it —
+// they differ here, so keep both for `sdk.host.getOwn` lookups.
+export const uiHostId = 'ui-multi'
+export const uiInterfaceId = 'ui'
+
 // Hardcoded in the upstream calcom/cal.com:v6.2.0 image at build time.
 // Cal's start.sh runs replace-placeholder.sh to rewrite static .next/ assets
 // from this value to whatever NEXT_PUBLIC_WEBAPP_URL is set at runtime.
 export const builtWebappUrl = 'http://localhost:3000'
 
 export async function getNonLocalUrls(effects: T.Effects): Promise<string[]> {
-  return sdk.serviceInterface
-    .getOwn(effects, 'ui', (i) => i?.addressInfo?.nonLocal.format() || [])
+  return sdk.host
+    .getOwn(effects, uiHostId, (host) => {
+      const iface = Object.values(host?.bindings ?? {})
+        .flatMap((b) => Object.values(b.interfaces))
+        .find((i) => i.id === uiInterfaceId)
+      return iface?.addressInfo.nonLocal.format() || []
+    })
     .const()
 }

Start9Labs/immich-startos

startos/utils.ts
@@ -5,9 +5,19 @@ import { sdk } from './sdk'
 
 export const uiPort = 2283 as const
 
+// Host ids (the sdk.MultiHost.of groups) — distinct from the interface ids exported on them.
+export const uiHostId = 'ui-multi'
+export const uiInterfaceId = 'ui'
+
 export async function getNonLocalUrls(effects: T.Effects): Promise<string[]> {
-  return sdk.serviceInterface
-    .getOwn(effects, 'ui', (i) => i?.addressInfo?.nonLocal.format() || [])
+  return sdk.host
+    .getOwn(effects, uiHostId, (host) => {
+      if (!host) return []
+      const ui = Object.values(host.bindings)
+        .flatMap((b) => Object.values(b.interfaces))
+        .find((i) => i.id === uiInterfaceId)
+      return ui?.addressInfo.nonLocal.format() || []
+    })
     .const()
 }
 export const POSTGRES_PATH = '/var/lib/postgresql' as const
@@ -86,14 +96,14 @@ export async function createCoreSubs(
   serverMountsArg: Parameters<typeof sdk.SubContainer.of>[2],
 ) {
   return {
-    postgresSub: await getPostgresSub(effects),
-    valkeySub: await sdk.SubContainer.of(
+    postgresSub: getPostgresSub(effects),
+    valkeySub: sdk.SubContainer.of(
       effects,
       { imageId: 'valkey' },
       sdk.Mounts.of(),
       'valkey',
     ),
-    mlSub: await sdk.SubContainer.of(
+    mlSub: sdk.SubContainer.of(
       effects,
       { imageId: 'immich-ml' },
       sdk.Mounts.of().mountVolume({
@@ -104,7 +114,7 @@ export async function createCoreSubs(
       }),
       'immich-ml',
     ),
-    serverSub: await sdk.SubContainer.of(
+    serverSub: sdk.SubContainer.of(
       effects,
       { imageId: 'immich-server' },
       serverMountsArg,